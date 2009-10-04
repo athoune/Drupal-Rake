f96f7d8e22aa80354c22305cebc3828b5cd4a1cd
Mathieu Lecarme <mathieu@garambrogne.net>
2009-10-04 19:05:38 +0200

    load

--- rakelib/db.rb
+++ rakelib/db.rb
@@ -1,5 +1,6 @@
 class Db
 	attr :uri
+	
 	def initialize(server, url)
 		@uri = URI.parse(url)
 		@bin = case server
@@ -10,11 +11,13 @@ class Db
 		@login, @password = @uri.userinfo.split(':')
 		@dbname = @uri.path[1,@uri.path.length]
 	end
+	
 	def dump(name)
 		sh "mkdir -p dump"
 		sh "#{@bin}mysqldump -u #{@login} -h #{@uri.host} --lock-tables --password='#{@password}' --quick --add-drop-database #{@dbname} | bzip2 -c > dump/#{name}.sql.bz2"
 	end
+	
 	def load(name)
-		
+		sh "bzcat dump/#{name}.sql.bz2 | #{@bin}mysql -h #{@uri.host} -u #{@login} --password='#{@password}' #{@dbname}"
 	end
 end
