5326a055bebc253ed8400562561644bdf7c5426f
Mathieu Lecarme <mathieu@garambrogne.net>
2010-03-07 23:52:30 +0100

    default profile

--- rakelib/profile.rb
+++ rakelib/profile.rb
@@ -1,6 +1,7 @@
 require 'yaml'
+require 'ftools'
 require 'rubygems'
-gem 'deep_merge'
+require 'deep_merge'
 
 module Profile
 
@@ -9,13 +10,15 @@ module Profile
 		uname = `uname`.strip.downcase
 	
 		if not File.exist? "profile.yml" or File.zero? "profile.yml"
-			raise StandardError, "You have to create a non empty profile.yml"
+			File::copy 'rakelib/default/profile.yml', '.'
+			raise StandardError, "A dummy profile was just created, modify it now"
 		end
 		profile = YAML::load(IO.read("profile.yml"))
 
 		cnf = "profile.#{name}.#{uname}.yml"
 		if ! File.exist?(cnf)
 			cnf = "profile.#{name}.yml"
+			File.new(cnf, 'w').close if ! File.exist?(cnf)
 		end
 		if File.exist?(cnf)
 			profile = profile.deep_merge!(YAML::load(IO.read(cnf)))
